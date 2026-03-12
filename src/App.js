45762341ba947fd09bba1d06e5bdd10b7260c744
GwizzK017 <21100471@virtualwindow.co.za>
2026-03-12 09:22:57 +0200

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import ComparePage from "./pages/ComparePage";
 import TimelinePage from "./pages/TimelinePage";
 
-import './App.css';
 import Navbar from "./components/Navbar";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
+import "./App.css";
 
 function App() {
   return (
-    
-    <div>
 
-      <Navbar />
-
-      { (
     <BrowserRouter>
 
       <Navbar />
@@ -49,11 +22,8 @@ function App() {
       </Routes>
 
     </BrowserRouter>
-  )}
 
-    </div>
   );
 }
-
 
 export default App;
