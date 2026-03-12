f59f74f10c08e8680a4eabcbae327072ffb78ab7
GwizzK017 <21100471@virtualwindow.co.za>
2026-03-12 12:36:11 +0200

    got the api working. needs to be specific*

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,49 +4,75 @@ import { useEffect, useState } from "react";
 
 function LandingPage() {
 
-  const [player, setPlayer] = useState(null);
-
+  const [player, setPlayer] = useState();
+  const [data, setData] = useState([]);
   useEffect(() => {
 
-    fetch("https://v3.football.api-sports.io/players?search=Messi", {
-      headers: {
-        "x-apisports-key": "181b4fe6ab467f7df7d4e1d6f6a2ea05"
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        setPlayer(data.response[0]);
-      });
+fetch("https://v3.football.api-sports.io/players?team=31&season=2024", {
+  method: "GET",
+  headers: {
+    "x-apisports-key": "52aebd1e39b84ac26ae09e2f7fc54da2"
+  }
+})
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+  setPlayer(data.response[0]);
+  setData(data.response[0]);
+})
+.catch(err => console.log(err));
 
-  }, []);
+}, []);
 
-  return (
+//   var myHeaders = new Headers();
+// myHeaders.append("x-apisports-key", "52aebd1e39b84ac26ae09e2f7fc54da2");
 
-    <div>
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow'
+// };
 
-      <h1>PitchStats</h1>
-      <h2>Player Data Test</h2>
+// fetch("https://v3.football.api-sports.io/players?search=Mandzukic", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
 
-      {player && (
-        <div>
+//   }, []);
 
-          <h3>{player.player.name}</h3>
+return (
 
-          <img
-            src={player.player.photo}
-            alt="player"
-            width="120"
-          />
+  <div>
 
-          <p>Age: {player.player.age}</p>
-          <p>Nationality: {player.player.nationality}</p>
+    <h1>PitchStats</h1>
 
-        </div>
-      )}
+    <h1>Compare the best of Players</h1>
+    <h2>Player Data Test</h2>
 
-    </div>
+    {player && (
+      <div> 
 
-  );
+        <h3>{player.player.name}</h3>
+
+        <img
+          src={player.player.photo}
+          alt="player"
+          width="150"
+          height="150"
+        />
+
+        <p>Age: {player.player.age}</p>
+        <p>Nationality: {player.player.nationality}</p>
+        <p>Height: {player.player.height}</p>
+         <p>Weight: {player.player.weight}</p>
+          <p>League: {player.player.league}</p>
+
+      </div>
+    )}
+
+  </div>
+
+);
 }
 
 export default LandingPage;
