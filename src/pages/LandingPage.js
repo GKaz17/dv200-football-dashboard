116b085d5970bfbb8e7cb55533720edb3ee57a4d
GwizzK017 <21100471@virtualwindow.co.za>
2026-03-30 21:53:39 +0200

    .

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-// LandingPage.jsx
+// LandingPage.js
 import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TbChartArrows } from "react-icons/tb";
